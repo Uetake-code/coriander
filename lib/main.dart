52819dc7f27010cbc52184f2132cdaff5fa9a712
Katsuaki <rugby.09try.sh@gmail.com>
2020-06-14 18:58:15 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:coriander/nect_page.dart';
-import 'package:flutter/foundation.dart';
+import 'package:coriander/main_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,81 +9,36 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('コリアンダー'),
+          ),
+          body: Consumer<MainModel>(
+              builder: (context, model, child) {
+                return Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        model.kboyText,
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                      RaisedButton(
+                        child: Text('ボタン'),
+                        onPressed: () {
+                          //ここで何
+                          model.changeKboyText();
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              }),
+             ),
+           ),
+         );
+       }
 }
-
- class MyHomePage extends StatefulWidget {
-   MyHomePage({Key key, this.title}) : super(key: key);
-   final String title;
-
-   @override
-   _MyHomePageState createState() => _MyHomePageState();
- }
-
- class _MyHomePageState extends State<MyHomePage> {
-   final items = List<String>.generate(10000, (i) => "Item $i");
-
-
-
-   @override
-   Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('katsuaki univerciter'),
-      ),
-      body: Container(
-        width: double.infinity,
-        child:ListView(
-          // This next line does the trick.
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Expanded(
-                  child: Image.network(
-                      'https://yt3.ggpht.com/a/AATXAJx2LQwh1kNX09biNyCenCNGzh5Rvwoue-an_Q=s176-c-k-c0xffffffff-no-rj-mo'
-                  ),
-                ),
-                Text ('KBOY'),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Expanded(
-                  child: Image.network(
-                      'https://yt3.ggpht.com/a/AATXAJx2LQwh1kNX09biNyCenCNGzh5Rvwoue-an_Q=s176-c-k-c0xffffffff-no-rj-mo'
-                  ),
-                ),
-                Text ('KBOY'),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Expanded(
-                  child: Image.network(
-                      'https://yt3.ggpht.com/a/AATXAJx2LQwh1kNX09biNyCenCNGzh5Rvwoue-an_Q=s176-c-k-c0xffffffff-no-rj-mo'
-                  ),
-                ),
-                Text ('KBOY'),
-              ],
-            ),
-            Container(
-              width: 160.0,
-              color: Colors.yellow,
-            ),
-            Container(
-              width: 160.0,
-              color: Colors.orange,
-            ),
-          ],
-        )
-      ),
-    );
-   }
- }
-
